8c6f846902010a46a2151b05b62493373b306091
MMR <moj2raj2@gmail.com>
2024-01-18 03:57:06 +0330

    init project

--- resources/js/Layouts/Scaffold.vue
+++ resources/js/Layouts/Scaffold.vue
@@ -29,10 +29,7 @@
        xmlns:xlink="http://www.w3.org/1999/xlink">
     <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
       <g transform="translate(-1.000000, -14.000000)" fill-rule="nonzero">
-        <g class="wave " fill="#fff">
-          <path
-              d="M 106 -20 C 393 16 493 33 584 39 C 1306 33 1402 29 1444 34 L 1441.191 104.352 L 1.121 104.031 Z"></path>
-        </g>
+
         <g transform="translate(0.000000, 50.000000)" class="fill-primary-200" fill="" fill-rule="nonzero">
           <path
               d="M 0.457 34.035 C 72 39 99 36 217 45 C 342 31 681 36 1216 12 C 1291 20 1402 29 1444 34 L 1441.191 104.352 L 1.121 104.031 L 0.457 34.035 Z"></path>
