c5f50db0ef3a8c9c6c330371036bda790b73f486
MMR <moj2raj2@gmail.com>
2024-02-07 05:50:28 +0330

    init project

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Helpers\SMSHelper;
+use App\Http\Helpers\SmsHelper;
 use App\Http\Helpers\Util;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -13,12 +13,12 @@ class MainController extends Controller
 {
     public function sendSms(Request $request)
     {
-        if (SMSHelper::checkRepeatedSMS("$request->phone", 5))
-            return response(['message' => sprintf(__('you_received_sms_in_n_minutes'),5),], 401);
+        if (SmsHelper::checkRepeatedSMS("$request->phone", 5))
+            return response(['message' => sprintf(__('you_received_sms_in_n_minutes'), 5),], 401);
         $code = Util::generateRandomNumber(5);
-        $res = (new SMSHelper())->Send("$request->phone", "$code", $request->type);
+        $res = (new SmsHelper())->Send("$request->phone", "$code", $request->type);
         if ($res) {
-            SMSHelper::addCode($request->phone, $code);
+            SmsHelper::addCode($request->phone, $code);
             return response(['message' => __('sms_send_to_phone')]);
         }
         return response(['message' => __('sms_send_to_phone')]);
