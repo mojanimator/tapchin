de07ddbe08fe2709cda2dc6ebf8c99e203789e60
MMR <moj2raj2@gmail.com>
2024-03-06 04:57:22 +0330

    add delivery timestamp

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -340,7 +340,7 @@ class CartController extends Controller
                     'repo_id' => $repo->id,
                     'agency_id' => $repo->agency_id,
                     'error_message' => $errorMessage,
-                    'has_available_shipping' => boolval($shipments[$idx]['has_available_shipping']) ?? false,
+                    'has_available_shipping' => boolval($shipments[$idx]['has_available_shipping'] ?? false),
                     'allow_visit' => optional($repo)->allow_visit ?? false,
                     'visit_checked' => boolval($cartItem->visit_checked) ?? false,
                 ];
