bafeece3dbd00adbb8500f8d5786a183a1eeeadd
MMR <moj2raj2@gmail.com>
2024-03-21 06:08:11 +0330

    add payment confirm

--- app/Http/Helpers/Pay.php
+++ app/Http/Helpers/Pay.php
@@ -175,7 +175,7 @@ class Pay
                     elseif (!empty($result['errors']))
                         return ['status' => 'danger', 'message' => $result['errors']['message']];
                     else
-                        return ['status' => 'danger', 'message' => $response->status()];
+                        return ['status' => 'danger', 'message' => $result];
                     break;
             }
         } catch (\Exception $e) {
