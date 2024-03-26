66a9c12f86930f8a96864969fdade380a938f44a
MMR <moj2raj2@gmail.com>
2024-03-26 20:50:07 +0330

    edit variation image

--- app/Http/Helpers/Variable.php
+++ app/Http/Helpers/Variable.php
@@ -194,10 +194,10 @@ class Variable
         return [
 
             ['id' => 1, 'fullname' => 'مدیر مرکزی', 'phone' => '09351414815', 'status' => 'active', 'role' => 'owner', 'agency_id' => 1, 'agency_level' => '0',
-                'access' => json_encode(['all']), 'email' => 'moj2raj2@gmail.com', 'password' => Hash::make('gX4ntH4RtIg$'), 'email_verified_at' => Carbon::now(), 'created_at' => Carbon::now(), 'phone_verified' => true,
+                'access' => json_encode(['all']), 'email' => 'moj2raj2@gmail.com', 'password' => Hash::make(env('ADMIN_PASSWORD')), 'email_verified_at' => Carbon::now(), 'created_at' => Carbon::now(), 'phone_verified' => true,
             ],
             ['id' => 2, 'fullname' => 'سهیل لطیفی', 'phone' => '09121391009', 'status' => 'active', 'role' => 'owner', 'agency_id' => 10, 'agency_level' => '3',
-                'access' => json_encode(['all']), 'email' => null, 'password' => Hash::make('09121391009'), 'email_verified_at' => Carbon::now(), 'created_at' => Carbon::now(), 'phone_verified' => true,
+                'access' => json_encode(['all']), 'email' => null, 'password' => null, 'email_verified_at' => Carbon::now(), 'created_at' => Carbon::now(), 'phone_verified' => true,
             ],
         ];
     }
