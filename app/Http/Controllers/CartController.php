eb1b938e3deaa8edd8c8dec36684e6fca5872e0b
MMR <moj2raj2@gmail.com>
2024-03-31 02:38:08 +0330

    edit agency profit

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -85,7 +85,7 @@ class CartController extends Controller
         $cart = $cart ?? Cart::create([
             'user_id' => optional($user)->id,
             'ip' => $ip,
-            'last_activity' => Carbon::now()->getTimestamp(),
+            'last_activity' => time(),
             'order_id' => null,
         ]);
         //set cart address
