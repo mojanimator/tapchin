502781fdb6822d3845c541317765740f791daa39
MMR <moj2raj2@gmail.com>
2024-04-01 21:47:05 +0330

    edit change repo variation

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -130,6 +130,7 @@ class BotController extends Controller
 //        $USER_EDIT = "ویرایش اطلاعات";
 //        $USER_REGISTER = " ثبت نام ";
 //        $CANCEL_REGISTER = "لغو ثبت نام";
+            Telegram::sendMessage(Telegram::LOGS[0], print_r($update, true));
 
             if ($tc == 'private') {
                 $this->user = Admin::where('telegram_id', $from_id)->first();
