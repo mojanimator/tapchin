3637f8c35dd1458165e7b63317df724eaa55e9d9
MMR <moj2raj2@gmail.com>
2024-04-01 22:07:33 +0330

    edit change repo variation

--- app/Http/Helpers/Telegram.php
+++ app/Http/Helpers/Telegram.php
@@ -16,6 +16,23 @@ use Morilog\Jalali\Jalalian;
 class Telegram
 {
     const LOGS = [72534783, 1212754313, -1002000102729];
+    const TOPIC_LOGS = 330;
+    const TOPIC_BUGS = 323;
+    const TOPIC_CHATS = 330;
+    const TOPIC_DESKTOP = 326;
+
+    static function sendBug($chat_id, $text, $mode = null, $reply = null, $keyboard = null, $disable_notification = false)
+    {
+        return self::creator('sendMessage', [
+            'chat_id' => $chat_id,
+            'text' => $text,
+            'parse_mode' => $mode,
+            'reply_to_message_id' => $reply,
+            'reply_markup' => $keyboard,
+            'disable_notification' => $disable_notification,
+            'message_thread_id' => self::TOPIC_BUGS,
+        ]);
+    }
 
     static function sendMessage($chat_id, $text, $mode = null, $reply = null, $keyboard = null, $disable_notification = false)
     {
@@ -26,6 +43,7 @@ class Telegram
             'reply_to_message_id' => $reply,
             'reply_markup' => $keyboard,
             'disable_notification' => $disable_notification,
+            'message_thread_id' => self::TOPIC_LOGS,
         ]);
     }
 
@@ -83,11 +101,15 @@ class Telegram
     }
 
 
-    static function logAdmins($msg, $mode = null)
+    static function logAdmins($msg, $mode = null, $topic = self::TOPIC_LOGS)
     {
         $res = null;
-        foreach ([self::LOGS[2]] as $log)
-            $res = self::sendMessage($log, $msg, $mode);
+        if ($topic == self::TOPIC_BUGS)
+            foreach ([self::LOGS[2]] as $log)
+                $res = self::sendBug($log, $msg, $mode);
+        else
+            foreach ([self::LOGS[2]] as $log)
+                $res = self::sendMessage($log, $msg, $mode);
         return $res;
 
     }
