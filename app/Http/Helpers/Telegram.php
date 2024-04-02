651160a0e557507085ad7a7aa493dc42766d33c4
MMR <moj2raj2@gmail.com>
2024-04-02 03:53:10 +0330

    edit change repo variation

--- app/Http/Helpers/Telegram.php
+++ app/Http/Helpers/Telegram.php
@@ -623,8 +623,7 @@ class Telegram
                     $msg .= " 🚚 " . "شهرها: " . $cities->whereIn('id', $data->cities ?? [])->pluck('name')->join(',') . PHP_EOL;
                     $msg .= "\xD8\x9C" . "➖➖➖➖➖➖➖➖➖➖➖" . PHP_EOL;
                     $msg .= " 📅 " . "ساعات ارسال: " . PHP_EOL;
-                    $msg .= print_r($data->getAttribute('timestamps'), true);
-//                    $msg .= collect($data->timestamps ?? [])->map(fn($e) => $e['from'] . '-' . $e['to'] . ($e['active'] ? "✅" : "⛔️"))->join("➖");
+                    $msg .= collect($data->getAttribute('timestamps') ?? [])->map(fn($e) => $e['from'] . '-' . $e['to'] . ($e['active'] ? "✅" : "⛔️"))->join("➖");
 
                     break;
                 case 'site_created':
