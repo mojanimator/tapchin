1d17081a95f60f0db23be20a1077771dad14c89e
MMR <moj2raj2@gmail.com>
2024-04-10 11:31:17 +0330

    edit change image resize

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -51,6 +51,7 @@ window.onload = (event) => {
         }
     } catch (_) {
     }
+    initChat();
 
 }
 
@@ -192,3 +193,17 @@ window.e2f = function (str) {
 
 
 };
+
+window.initChat = function () {
+    var i = "5EEMY6", a = window, d = document;
+
+    function g() {
+        var g = d.createElement("script"), s = "https://www.goftino.com/widget/" + i,
+            l = localStorage.getItem("goftino_" + i);
+        g.async = !0, g.src = l ? s + "?o=" + l : s;
+        d.getElementsByTagName("head")[0].appendChild(g);
+    }
+
+    "complete" === d.readyState ? g() : a.attachEvent ? a.attachEvent("onload", g) : a.addEventListener("load", g, !1);
+
+}
