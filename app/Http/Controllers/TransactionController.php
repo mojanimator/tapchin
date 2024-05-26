6c80ce219f156cc3e9a2e7f3113668b04603a2bf
MMR <moj2raj2@gmail.com>
2024-05-26 18:39:41 +0330

    adding api

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -304,10 +304,11 @@ class TransactionController extends Controller
                 $query->where('to_type', 'user')->where('to_id', $userAdmin->id)->whereNotNull('payed_at');;
             });
         }
+//        $query->orWhere('title', 'like', "%$search%");
         if ($search)
-            $query = $query->where(function ($query) use ($search) {
-                $query->orWhere('title', 'like', "%$search%");
-//                    ->orWhere('pay_id', 'like', "%$search%")
+            $query->where(function ($query) use ($search) {
+                $query->orWhere('title', 'like', "%$search%")
+                    ->orWhere('pay_id', 'like', "%$search%");
             });
 
 
