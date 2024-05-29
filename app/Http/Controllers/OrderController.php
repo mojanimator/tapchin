aae614046f892bfac0909a237745059a3e62e5a4
MMR <moj2raj2@gmail.com>
2024-05-29 21:10:40 +0330

    adding api

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -743,7 +743,7 @@ class OrderController extends Controller
             return $paginated->getCollection()->transform(
                 function ($item) {
                     $item->statuses = $item->getAvailableStatuses();
-                    $item->isPayable = !$item->payed_at && in_array($item->status, ['pending', 'ready']);
+                    $item->is_payable = !$item->payed_at && in_array($item->status, ['pending', 'ready', 'shipping']);
                     return $item;
                 }
 
